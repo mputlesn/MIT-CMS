c088b5c0da4b15c207f551026c229951e4f9f8f3
Nthabi <mputle15@gmail.com>
2019-02-13 11:56:19 +0200

    New Changes

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MitService } from '../../provider/mit.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-users',
@@ -10,11 +11,33 @@ import { Router } from '@angular/router';
 export class UsersComponent implements OnInit {
 
   users;
+  
 
   constructor(private mit: MitService, private router: Router) { }
 
   ngOnInit() {
+    
     this.getallUsers()
+    let timerInterval
+   Swal.fire({
+    title: 'Loading',
+    html: 'Please wait, still loading',
+    timer: 2000,
+    onBeforeOpen: () => {
+      Swal.showLoading()
+   
+    },
+    onClose: () => {
+      clearInterval(timerInterval)
+    }
+   }).then((result) => {
+    if (
+      // Read more about handling dismissals
+      result.dismiss === Swal.DismissReason.timer
+    ) {
+      console.log('I was closed by the timer')
+    }
+   })
   }
 
   logout(){
